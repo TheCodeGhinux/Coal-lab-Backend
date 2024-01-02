bea7a59dedc92aaa618040f7a3538fe6ace656b5
highb33kay <hiibeekayvibe@gmail.com>
2024-01-02 23:01:24 +0100

    Remove unused eventSlugify function

--- src/services/slugify.ts
+++ src/services/slugify.ts
@@ -19,24 +19,3 @@ export const slugify = async (string, num = 0) => {
     return newSlug;
   }
 };
-
-// event slug
-export const eventSlugify = async (string, num = 0) => {
-  let newSlug = string
-    .toLowerCase()
-    .replace(/[^\w ]+/g, "")
-    .replace(/[ ]+/g, "-");
-  newSlug = num > 0 ? `${newSlug}-` + num : newSlug;
-
-  const event = await prisma.event.findUnique({
-    where: {
-      eventSlug: newSlug,
-    },
-  });
-
-  if (event) {
-    return eventSlugify(string, num + 1);
-  } else {
-    return newSlug;
-  }
-};
